835997da45ff478db8397329aaea1a57fbd7c239
ASWIN7833 <7833sai@gmail.com>
2026-02-17 09:08:56 +0530

    aswin change: in Connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header/Header";
 import Contact from "./components/Contact/Contact";
 import Products from "./components/Products/Products";
-
+import Connect from "./components/connect/Connect";
+import React from "react";
 function App() {
   return (
     <>
       <Header />
-      <Products />
-      <Contact/>
+      {/* <Products /> */}
+      {/* <Contact/> */}
+      <Connect />
     </>
   );
 }
--- src/components/ui/Heading/Heading.jsx
+++ src/components/ui/Heading/Heading.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Heading.css";
 
-function Heading({ heading, subHeading = "" }) {
+function Heading({ heading, subHeading = "",para }) {
   return (
     <div className="heading-container">
       <h1 className="heading">{heading}</h1>
       {subHeading && <h2 className="sub-heading">{subHeading}</h2>}
+      {para && <p className="paragraph">{para}</p>}
     </div>
   );
 }
