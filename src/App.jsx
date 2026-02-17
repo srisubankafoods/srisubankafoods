2ff88146fc542a36084219637a86688e7cd1b41b
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-02-17 08:21:16 +0530

    Add About component and integrate it into the main App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
 import Contact from "./components/Contact/Contact";
 import Products from "./components/Products/Products";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
       <Products />
+      <About/>
       <Contact/>
     </>
   );
---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Heading from '../ui/Heading/Heading'
+
+function About() {
+  return (
+    <div className='about-container'>
+        
+        <Heading heading="About" subHeading='Serving Happiness Since Day One.'/>
+    </div>
+  )
+}
+
+export default About
