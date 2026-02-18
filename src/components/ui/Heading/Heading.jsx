42c0f2dc5907861bfc45f5b43c8ee4728e05e580
ASWIN7833 <7833sai@gmail.com>
2026-02-18 12:32:51 +0530

    aswin change after updating header

--- src/components/ui/Heading/Heading.jsx
+++ src/components/ui/Heading/Heading.jsx
@@ -3,10 +3,10 @@ import "./Heading.css";
 
 function Heading({ heading, subHeading = "",para }) {
   return (
-    <div className="heading-container">
-      <h1 className="heading">{heading}</h1>
-      {subHeading && <h2 className="sub-heading">{subHeading}</h2>}
-      {para && <p className="paragraph">{para}</p>}
+    <div className="heading-container ">
+      <h1 className="heading ">{heading}</h1>
+      {subHeading && <h2 className="sub-heading ">{subHeading}</h2>}
+      {para && <p className="paragraph ">{para}</p>}
     </div>
   );
 }
