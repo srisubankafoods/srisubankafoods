3485fce8421d4667271bd3b69e1d018a8892e785
Arun Meyyappan P L <arunmeyyappanpl@gmail.com>
2026-02-19 11:25:02 +0530

    Update styles for text content and headings; adjust padding and font sizes

--- src/components/ui/Heading/Heading.jsx
+++ src/components/ui/Heading/Heading.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Heading.css";
-
-function Heading({ heading, subHeading = "",para="" }) {
+alert(window.innerWidth + "X" + window.innerHeight)
+function Heading({ heading ="", subHeading = "",para="" }) {
   return (
     <div className="heading-container ">
-      <h1 className="heading ">{heading}</h1>
+      {heading && <h1 className="heading ">{heading}</h1>}
       {subHeading && <h2 className="sub-heading ">{subHeading}</h2>}
       {para && <p className="paragraph ">{para}</p>}
     </div>
