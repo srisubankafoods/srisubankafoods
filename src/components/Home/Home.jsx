12167ab727ee5ff0bfaaf302809c131bb43177a4
ASWIN7833 <7833sai@gmail.com>
2026-02-20 22:18:16 +0530

    Correct capitalization in Home component header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     <>
     <div id="Home-page">
     <div className="homehead">
-      <h1 className='h1'>Eat smart. </h1>
+      <h1 className='h1'>Eat Smart. </h1>
       <h1 className='h11'>Eat Millets. </h1>
       <h4>"<em>Sustainable,healthy,and delicious choices for every day.</em>"</h4>
     </div>
