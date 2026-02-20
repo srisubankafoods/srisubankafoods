4d7ff1ffe76dc03018c4485203dc66880691d524
ASWIN7833 <7833sai@gmail.com>
2026-02-20 09:09:01 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/connect/Connect.jsx
+++ src/components/connect/Connect.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Connect.css";
 import Icon from "./Icons/Icon";
 import Heading from "../ui/Heading/Heading";
-import connectImage from "./images/connect3.jpeg";
+import connectImage from "./../../assets/images/connect3.jpeg";
 import { FaPhoneVolume } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import { SlGlobe } from "react-icons/sl";
