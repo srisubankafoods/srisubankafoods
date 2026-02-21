463d70991a94550ad3e4d6a82326d16e569a1168
ASWIN7833 <7833sai@gmail.com>
2026-02-21 13:18:28 +0530

    Fix spacing in tagline for improved readability in Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <div className="homehead">
       <h1 className='h1'>Eat Smart. </h1>
       <h1 className='h11'>Eat Millets. </h1>
-      <h4>"<em>Sustainable,healthy,and delicious choices for every day.</em>"</h4>
+      <h4>"<em>Sustainable, healthy and delicious choices for every day.</em>"</h4>
     </div>
     <Images />
     </div>
